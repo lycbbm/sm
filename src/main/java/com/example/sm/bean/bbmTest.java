f7586d29547cd99f016a6c795cd3a9b599f3d4d7
annabian <annabian@tencent.com>
2020-09-20 10:36:47 +0800

    commit test

---
+++ src/main/java/com/example/sm/bean/bbmTest.java
@@ -0,0 +1,12 @@
+package com.example.sm.bean;
+
+/**
+ * @description:
+ * @author: bbm
+ * @Date: 2020/9/20 10:25 上午
+ */
+public class bbmTest {
+    public static void main(String[] args) {
+        System.out.println("test");
+    }
+}
