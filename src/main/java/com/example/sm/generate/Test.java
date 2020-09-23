ae94faaf9f7fc22f1d3811c38b5129b8299a08c0
lycbbm <45055946+lycbbm@users.noreply.github.com>
2020-09-23 19:51:50 +0800

    Update Test.java

--- src/main/java/com/example/sm/generate/Test.java
+++ src/main/java/com/example/sm/generate/Test.java
@@ -31,4 +31,15 @@ public class Test {
         System.out.println("end codeData length:" + endData.length());
         return "summary";
     }
+    
+        private String getCodeContent4() throws IOException {
+        // 明文码体数据16 = 引导信息1 + 体系标识2 + 分类标识3 + ct1 + 码体版本标识17 + 码体签名
+        String ct1 = "getCt1()";
+        System.out.println("****getCodeContent ct2 2:" + ct1.length());
+        String codeData = "idxInf + tagCode + tagType + getCt1()";
+        String endData = codeData + "generateCodeTime + codeEffictiveTime + codeVersion + getCodeBodySign2()";
+        System.out.println("end codeData:" + endData);
+        System.out.println("end codeData length:" + endData.length());
+        return "summary";
+    }
 }
