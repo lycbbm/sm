3ebd30faeb73bd2ff3a3aa8605222ec2c255d2cf
lycbbm <45055946+lycbbm@users.noreply.github.com>
2020-09-23 19:49:46 +0800

    Update Test.java

--- src/main/java/com/example/sm/generate/Test.java
+++ src/main/java/com/example/sm/generate/Test.java
@@ -15,7 +15,7 @@ public class Test {
         System.out.println("****getCodeContent ct2 2:" + ct2.length());
         String codeData = "idxInf + tagCode + tagType + getCt2() + codeVersion";
 
-        System.out.println("end codeData:" + codeData + "test update");
+        System.out.println("end codeData:" + codeData + "test update" +"bbm");
         System.out.println("end codeData length:" + codeData.length());
 
         return codeData;
@@ -24,18 +24,6 @@ public class Test {
     private String getCodeContent2() throws IOException {
         // 明文码体数据16 = 引导信息1 + 体系标识2 + 分类标识3 + ct1 + 码体版本标识17 + 码体签名
         String ct1 = "getCt1()";
-        System.out.println("****getCodeContent ct2 2:" + ct1.length());
-        String codeData = "idxInf + tagCode + tagType + getCt1()";
-        String endData = codeData + "generateCodeTime + codeEffictiveTime + codeVersion + getCodeBodySign2()";
-        System.out.println("end codeData:" + endData);
-        System.out.println("end codeData length:" + endData.length());
-        return "summary";
-    }
-
-    private String getCodeContent3() throws IOException {
-        // 明文码体数据16 = 引导信息1 + 体系标识2 + 分类标识3 + ct1 + 码体版本标识17 + 码体签名
-        String ct1 = "getCt1()";
-        System.out.println("***getCodeContent ct2 1:" + ct1);
         System.out.println("****getCodeContent ct2 2:" + ct1.length());
         String codeData = "idxInf + tagCode + tagType + getCt1()";
         String endData = codeData + "generateCodeTime + codeEffictiveTime + codeVersion + getCodeBodySign2()";
