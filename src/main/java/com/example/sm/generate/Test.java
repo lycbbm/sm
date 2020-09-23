e324d9b6d38c58608782d152f8a7321e89b97793
annabian <annabian@tencent.com>
2020-09-23 19:39:00 +0800

    master update

--- src/main/java/com/example/sm/generate/Test.java
+++ src/main/java/com/example/sm/generate/Test.java
@@ -21,4 +21,17 @@ public class Test {
         return codeData;
     }
 
+    private String getCodeContent2() throws IOException {
+        // 明文码体数据16 = 引导信息1 + 体系标识2 + 分类标识3 + ct2 + 码体版本标识17
+        String ct2 = "getCt2()";
+        System.out.println("***getCodeContent ct2 1:" + ct2);
+        System.out.println("****getCodeContent ct2 2:" + ct2.length());
+        String codeData = "idxInf + tagCode + tagType + getCt2() + codeVersion";
+
+        System.out.println("end codeData:" + codeData + "test update");
+        System.out.println("end codeData length:" + codeData.length());
+
+        return codeData;
+    }
+
 }
