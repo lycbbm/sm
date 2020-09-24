40b718bee2fbf0059760fb8c0fbe445443c7519d
annabian <annabian@tencent.com>
2020-09-24 19:24:44 +0800

    测试合并

---
+++ src/main/java/com/example/sm/generate/Test.java
@@ -0,0 +1,29 @@
+package com.example.sm.generate;
+
+import com.example.sm.algorithm.password.SM2Utils;
+import com.example.sm.algorithm.password.Util;
+
+import java.io.IOException;
+
+/**
+ * @description:
+ * @author: bbm
+ * @Date: 2020/9/24 7:23 下午
+ */
+public class Test {
+    /**
+     * 验签
+     * @return
+     */
+    public static boolean verify(String summary,String sign, String pubk) {
+        boolean vs = false;
+        try {
+            vs = SM2Utils.verifySign("userId.getBytes()".getBytes(), Util.hexToByte(pubk),Util.hexToByte(summary), Util.hexToByte(sign));
+        } catch (IllegalArgumentException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return vs;
+    }
+}
