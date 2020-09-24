2764109e12a50fa5ca351a4eefbe51ba9b724d96
annabian <annabian@tencent.com>
2020-09-24 19:49:51 +0800

    测试合并

--- src/main/java/com/example/sm/generate/Test.java
+++ src/main/java/com/example/sm/generate/Test.java
@@ -18,8 +18,8 @@ public class Test {
     public static boolean verify(String summary,String sign, String pubk) {
         boolean vs = false;
         try {
-            vs = SM2Utils.verifySign("es".getBytes(), Util.hexToByte(pubk),Util.hexToByte(summary), Util.hexToByte(sign));
-            System.out.println("hahlou");
+            vs = SM2Utils.verifySign("test".getBytes(), Util.hexToByte(pubk),Util.hexToByte(summary), Util.hexToByte(sign));
+            System.out.println("test");
         } catch (IllegalArgumentException e) {
             e.printStackTrace();
         } catch (IOException e) {
